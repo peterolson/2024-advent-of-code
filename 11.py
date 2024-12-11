33c30c5e3ecf7498df291b9f996bb29894718542
Peter Olson <peter.e.c.olson+github@gmail.com>
2024-12-10 23:46:53 -0600

    use counter and generator function to be all fancy

--- 11.py
+++ 11.py
@@ -1,36 +1,32 @@
-with open('input/11.txt', 'r') as f:
-    lines = f.read().splitlines()
+from collections import Counter
 
-initial_stones = list(map(int, lines[0].split()))
-stone_dict = {s: initial_stones.count(s) for s in initial_stones}
+with open('input/11.txt', 'r') as f:
+    stones = list(map(int, f.readline().split()))
+
+stone_count = Counter(stones)
 
 def transform_stone(n):
-    if n == 0:
-        return [1]
     s = str(n)
-    if len(s) % 2 == 0:
+    if n == 0:
+        yield 1
+    elif len(s) % 2 == 0:
         half = len(s) // 2
-        return [int(s[:half]), int(s[half:])]
-    return [n * 2024]
+        yield int(s[:half])
+        yield int(s[half:])
+    else:
+        yield n * 2024
 
-def blink(stone_dict):
-    new_stones = {}
-    for s, count in stone_dict.items():
-        transformed = transform_stone(s)
-        for t in transformed:
-            if t in new_stones:
-                new_stones[t] += count
-            else:
-                new_stones[t] = count
+def blink(stone_count : Counter):
+    new_stones = Counter()
+    for s, count in stone_count.items():
+        for t in transform_stone(s):
+            new_stones[t] += count
     return new_stones
 
-def count_stones(stone_dict):
-    return sum(stone_dict.values())
-
-s = stone_dict
+s = stone_count
 for i in range(75):
     s = blink(s)
     if i + 1 == 25:
-        print("Part 1:", count_stones(s))
+        print("Part 1:", s.total())
     if i + 1 == 75:
-        print("Part 2:", count_stones(s))
+        print("Part 2:", s.total())
